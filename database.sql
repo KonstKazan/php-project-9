6a09cbc0856fe1b07c3361799a259c3ec66dada8
Konstantin <konst.kazan@gmail.com>
2024-09-24 20:03:37 +0300

    change type in database.sql

--- database.sql
+++ database.sql
@@ -1,15 +1,15 @@
 CREATE TABLE IF NOT EXISTS urls (
     id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     name VARCHAR(255) NOT NULL,
-    created_at VARCHAR(255) NOT NULL
+    created_at TIMESTAMP NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS url_checks (
     id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    url_id INTEGER NOT NULL,
-    status_code VARCHAR(255) NOT NULL,
-    h1 VARCHAR(255),
-    title VARCHAR(255),
-    description VARCHAR(255),
-    created_at VARCHAR(255) NOT NULL
+    url_id INTEGER REFERENCES urls(id) NOT NULL,
+    status_code SMALLINT NOT NULL,
+    h1 VARCHAR(1000),
+    title TEXT,
+    description TEXT,
+    created_at TIMESTAMP NOT NULL
 );
